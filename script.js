3b66c1f5d00555558f76507ec185c8187ff52451
Aaron <72465497+ManaCodes@users.noreply.github.com>
2022-04-11 22:28:16 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+const quotes= [
+    {
+      quote:
+        "Life is too short and sweet to be spent by cribbing and complaining about things. Here are some random quotes about the most wonderful gift that we've got",
+      author: " Life"
+    },
+  {
+   quote: "Programming is not a science. Programming is a craft.",
+  author: "Richard Stallman"  
+  },
+  {
+    quote: "Most good programmers do programming not because they expect to get paid or get adulation by the public, but because it is fun to program.",
+    author: "Linus Torvalds"
+  },
+   {
+    quote: "In real open source, you have the right to control your own destiny",
+    author: "Linus Torvalds"
+  },
+  {
+  quote: "We don't make mistakes; we just have happy accidents.",
+    
+    author: "Bob Ross"
+  }
+  
+  ];
+
+class Quote extends React.Component {
+constructor(props) {
+  super(props);
+  this.state = {
+   quote: '----',
+   author: '----'
+   };
+  
+  this.handleChange=this.handleChange.bind(this);
+}
+  
+  handleChange(){
+    let i= Math.floor(Math.random() * quotes.length)
+    this.setState({
+      quote: quotes[i].quote,
+      author: quotes[i].author
+    });
+  }
+  
+ render() {
+  return (
+    <div>
+    <div id="quote-box">
+      <div id="text"> 
+      <button id="new-quote" onClick={this.handleChange}>Click Me</button>
+      <p>Quote: {this.state.quote}</p>
+        <div id="author">
+        <p>Author: {this.state.author}</p>
+           
+         </div>
+       </div>
+     </div>
+     </div>
+  );
+ }
+}
+
+ReactDOM.render(<Quote />, document.getElementById("root"));
